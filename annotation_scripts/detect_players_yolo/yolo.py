eed0ebdc9812e93f0f6eb690f31049a5d3f4ebe0
Jess-dudu <jessica.dudu.chen@gmail.com>
2025-01-25 07:36:09 +0000

    yolo.py runs in scripts folder

--- annotation_scripts/detect_players_yolo/yolo.py
+++ annotation_scripts/detect_players_yolo/yolo.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
 # load a model
-model = YOLO("yolo11n-pose.pt")
+model = YOLO("../../_exp/yolo11n-pose.pt")
 
 # predict with model
-results = model.predict(source="20240312_203520.MOV", show=False, save=True)
+project = "../../_exp/yolo_run"
+results = model.predict(source="../../_exp/20240312_203520.MOV", show=False, save=True, project=project, name="predict")
